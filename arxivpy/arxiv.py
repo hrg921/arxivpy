3e46b520e99b446ebcdc8f07f2c5ca5030a1c280
hrg921 <hrg921@gmail.com>
2018-07-20 16:03:56 +0900

    customize to use in Cerebro.Scholar

--- arxivpy/arxiv.py
+++ arxivpy/arxiv.py
@@ -119,42 +119,7 @@ def query(search_query=['cs.CV', 'cs.LG', 'cs.CL', 'cs.NE', 'stat.ML'],
             if entry['title'] == 'Error':
                 print('Error %s' % entry['summary'])
                 print('Check query %s from the website if it returns anything' % (base_url + query))
-            main_term = entry['arxiv_primary_category']['term']
-            terms = '|'.join([tag['term'] for tag in entry['tags']])
-            main_author = entry['author']
-            authors = ', '.join([author['name'].strip() for author in entry['authors']])
-            url = entry['link']
-            for e in entry['links']:
-                if 'title' in e.keys():
-                    if e['title'] == 'pdf':
-                        pdf_url = e['href']
-                else:
-                    pdf_url = 'http://arxiv.org/pdf/%s' % url.split('/abs/')[-1]
-            if 'arxiv_comment' in entry.keys():
-                comment = entry['arxiv_comment']
-            else:
-                comment = 'No comment found'
-            if 'journal_ref' in entry.keys():
-                journal_ref = entry['journal_ref']
-            else:
-                journal_ref = 'No journal ref found'
-
-            title = entry['title_detail']['value'].replace('\n', ' ').strip()
-            abstract = entry['summary'].replace('\n', ' ')
-            publish_date = parser.parse(entry['published'])
-            article = {'id': url.split('/abs/')[-1],
-                       'term': main_term,
-                       'terms': terms,
-                       'main_author': main_author,
-                       'authors': authors,
-                       'url': url,
-                       'pdf_url': pdf_url,
-                       'title': title,
-                       'abstract': abstract,
-                       'publish_date': publish_date,
-                       'comment': comment,
-                       'journal_ref': journal_ref}
-            articles.append(article)
+            articles.append(entry)
         if i > start_index: time.sleep(wait_time + random.uniform(0, 3))
         articles_all.extend(articles)
     return articles_all
